c39f56be65caccf864263413054f1f8ea9272336
iaifrns <nsinifranc@gmail.com>
2024-02-23 14:33:11 +0100

    concluded the basic front

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -13,30 +13,25 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 import { Spacer } from "../common/spacer";
 
 export const ContactForm = () => {
-
-    const theme = useTheme();
-    const match = useMediaQuery(theme.breakpoints.down('md'));
+  const theme = useTheme();
+  const match = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
-    <Stack
-      direction={"column"}
-      justifyContent={"center"}
-      alignItems={"center"}
-    >
+    <Stack direction={"column"} justifyContent={"center"} alignItems={"center"}>
       <CompanyInfo match={match} />
-      <Spacer/>
+      <Spacer />
       <Form match={match} />
     </Stack>
   );
 };
 
-const CompanyInfo = ({match} : {match : boolean}) => {
+const CompanyInfo = ({ match }: { match: boolean }) => {
   return (
     <Stack
-      direction={ match ? 'column' : "row"}
+      direction={match ? "column" : "row"}
       width={"80%"}
       justifyContent={"space-between"}
-      alignItems={match ? 'center' : "start"}
+      alignItems={match ? "center" : "start"}
       gap={2}
     >
       <Box
@@ -61,7 +56,7 @@ const CompanyInfo = ({match} : {match : boolean}) => {
         gap={1}
       >
         <Avatar sx={{ bgcolor: colors.secondary }}>
-          <Icon icon="subway:location" />
+          <Icon icon="majesticons:phone" />
         </Avatar>
         <Typography> Phone: 00447877020977</Typography>
       </Box>
@@ -73,7 +68,7 @@ const CompanyInfo = ({match} : {match : boolean}) => {
         gap={1}
       >
         <Avatar sx={{ bgcolor: colors.secondary }}>
-          <Icon icon="subway:location" />
+          <Icon icon="clarity:email-solid" />
         </Avatar>
         <Typography>Email : takcooling@gmail.com</Typography>
       </Box>
@@ -81,30 +76,36 @@ const CompanyInfo = ({match} : {match : boolean}) => {
   );
 };
 
-const Form = ({match} : {match : boolean}) => {
+const Form = ({ match }: { match: boolean }) => {
   return (
     <Stack
-      direction={ match ? 'column' : "row"}
+      direction={match ? "column" : "row"}
       width={"80%"}
       boxShadow={match ? 0 : 1}
-      border={ match ? 0 : "1px solid lightgray"}
+      border={match ? 0 : "1px solid lightgray"}
     >
       <Stack
         direction={"column"}
-        width={match ? '100%' : "60%"}
+        width={match ? "100%" : "60%"}
         bgcolor={colors.white}
         p={match ? 0 : 4}
         gap={5}
       >
-        <Typography variant="h4" display={match ? 'none' : 'block'}>Contact Us</Typography>
-        <Stack direction={ match ? 'column' : "row"} gap={ match ? 4 : 1} width={"100%"}>
-          <Box width={match ? '100%' : "50%"}>
+        <Typography variant="h4" display={match ? "none" : "block"}>
+          Contact Us
+        </Typography>
+        <Stack
+          direction={match ? "column" : "row"}
+          gap={match ? 4 : 1}
+          width={"100%"}
+        >
+          <Box width={match ? "100%" : "50%"}>
             <Typography variant="body2" fontWeight={"bold"}>
               FULL NAME
             </Typography>
             <TextField placeholder="Name" fullWidth variant="standard" />
           </Box>
-          <Box width={match ? '100%' : "50%"}>
+          <Box width={match ? "100%" : "50%"}>
             <Typography variant="body2" fontWeight={"bold"}>
               EMAIL ADDRESS
             </Typography>
@@ -133,7 +134,11 @@ const Form = ({match} : {match : boolean}) => {
           <Button variant="contained">Send Message</Button>
         </Box>
       </Stack>
-      <Stack display={match ? 'none' : 'flex'} direction={"column"} width={"40%"}>
+      <Stack
+        display={match ? "none" : "flex"}
+        direction={"column"}
+        width={"40%"}
+      >
         <img
           src="https://images.pexels.com/photos/7658355/pexels-photo-7658355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           height={"100%"}
